d03700a28fc640843f760c71d75a5f5f6eac1668
Kouhei Sutou <kou@clear-code.com>
2018-04-11 14:08:25 +0900

    Update document

---
+++ docs/static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '8.0.1',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt'
+};

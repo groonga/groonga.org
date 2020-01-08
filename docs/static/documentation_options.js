cb4f65782c766641f385c3b524f6de085137b9f2
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-01-08 13:31:16 +0900

    Update documents for 9.1.1

--- docs/static/documentation_options.js
+++ docs/static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '9.1.0',
+    VERSION: '9.1.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
